42bc6c4df7c3f196a18191caeaee0944c3f2b104
Elvin <elvin4munka@gmail.com>
2020-09-16 18:03:29 +0300

    HW1: Implement clear method in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,8 +3,13 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
