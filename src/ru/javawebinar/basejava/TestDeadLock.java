6b3bbb614866db35e3d94e43623a7d496dc2add0
Elvin <elvin4munka@gmail.com>
2021-02-02 23:21:56 +0300

    HW11: Refactor

--- src/ru/javawebinar/basejava/TestDeadLock.java
+++ src/ru/javawebinar/basejava/TestDeadLock.java
@@ -11,12 +11,17 @@ public class TestDeadLock {
 
     public static void getDeadLock(Object lock1, Object lock2) {
         String thread = Thread.currentThread().getName();
-        System.out.println("Ожидание захвата " + lock1 + " монитора потоком " + thread);
+        System.out.println(thread + " is waiting for monitor capture " + lock1);
         synchronized (lock1) {
-            System.out.println("Захват " + lock1 + " монитора потоком " + thread);
-            System.out.println("Ожидание захвата " + lock2 + " монитора потоком " + thread);
+            System.out.println(lock1 + " locked by " + thread);
+            System.out.println(thread + " is waiting for monitor capture " + lock2);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             synchronized (lock2) {
-                System.out.println("Захват " + lock2 + " монитора потоком " + thread);
+                System.out.println(lock1 + " and " + lock2 + " locked by " + thread);
             }
         }
     }
